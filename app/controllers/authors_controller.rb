687b4196b67fecd3f64893daacd3c1ed6709e0ca
Bruno <brunosilvabrait@gmail.com>
2023-04-22 22:53:01 -0300

    Edit de Posts e Edit de Authors, Correção nos controllers

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,7 +4,6 @@ class AuthorsController < ApplicationController
   before_action :load_author, only: %i[update destroy]
 
   def update
-    @author = Author.find(params[:id])
     @author.attributes = author_params
     @author.save!
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,39 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :load_post, only: %i[update show destroy]
+  before_action :load_post, only: %i[update show destroy edit]
 
   def index
     @posts = Post.includes(:comments).order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def create
-    @post = Post.new
-    @post.attributes = post_params
+    @post = Post.new(post_params)
     @post.author_id = current_author.id
     redirect_to posts_path if @post.save
   end
 
   def show; end
 
+  def edit; end
+
   def update
-    @post.attributes = post_params
-    @post.save!
+    @post.attributes = post_params_update
+    redirect_to posts_path if @post.save!
   end
 
   def destroy
-    @post.destroy!
+    redirect_to posts_path if @post.destroy
   end
 
   private
 
   def load_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params_update
+    params.require(:post).permit(:body)
   end
 
   def post_params
